ddfec5b4fce179867cb1e9d6de6e58983992ed17
Andrea Giacobino <no.andrea@gmail.com>
2020-12-22 17:03:11 +0100

    maybe batter creation?

--- src/views/EventsCreate.vue
+++ src/views/EventsCreate.vue
@@ -1,13 +1,7 @@
 <template>
-  <v-form
-    @submit.prevent="submit"
-    v-model='valid'
-    ref="form"
-  >
+  <v-form @submit.prevent="submit" v-model="valid" ref="form">
     <h1>Create Event</h1>
-    <v-card
-      class="my-4"
-    >
+    <v-card class="my-4">
       <v-card-title>Event</v-card-title>
       <v-card-text>
         <v-container>
@@ -16,24 +10,23 @@
               <v-text-field
                 v-model="model.token_symbol"
                 label="Token Symbol"
+                placeholder="DROPS"
               ></v-text-field>
-              <v-text-field
+              <!-- <v-text-field
                 v-model="model.owner"
                 label="Owner Email"
-              ></v-text-field>
-              <v-text-field
+              ></v-text-field> -->
+              <!-- <v-text-field
                 v-model="model.provider"
                 label="Provider"
-              ></v-text-field>
+              ></v-text-field> -->
             </v-col>
           </v-row>
         </v-container>
       </v-card-text>
     </v-card>
-    <v-card
-      class="my-4"
-    >
-      <v-card-title>Event Payload</v-card-title>
+    <v-card class="my-4">
+      <!-- <v-card-title>Event Payload</v-card-title>
       <v-card-text>
         <v-container>
           <v-row justify="space-between">
@@ -61,44 +54,38 @@
             </v-col>
           </v-row>
         </v-container>
-      </v-card-text>
+      </v-card-text> -->
     </v-card>
 
     <h2>Event Genesis Accounts</h2>
     <v-container>
       <v-row>
         <v-col
-          cols="12" md="4"
-          v-for='(g,n) in model.genesis_accounts'
-          :key='g.name'
+          cols="12"
+          md="4"
+          v-for="(g, n) in model.genesis_accounts"
+          :key="g.name"
         >
           <v-card>
             <v-card-text>
-              <v-text-field
-                v-model="g.name"
-                label="Name"
-              ></v-text-field>
+              <v-text-field v-model="g.name" label="Name"></v-text-field>
               <v-text-field
                 v-model="g.genesis_balance"
                 label="Genesis Balance"
+                :value="`500${model.tokenSymbol},1000000evtx,100000000stake`"
               ></v-text-field>
               <v-switch
                 v-model="g.faucet"
                 :label="`Faucet: ${g.faucet.toString()}`"
+                :value="false"
               ></v-switch>
               <v-switch
                 v-model="g.validator"
                 :label="`Validator: ${g.validator.toString()}`"
+                :value="false"
               ></v-switch>
-              <v-btn
-                @click='removeGenesis(n)'
-                class="mx-2"
-                dark
-                color="red"
-              >
-                <v-icon dark>
-                  mdi-minus
-                </v-icon>
+              <v-btn @click="removeGenesis(n)" class="mx-2" dark color="red">
+                <v-icon dark> mdi-minus </v-icon>
                 remove
               </v-btn>
             </v-card-text>
@@ -106,36 +93,23 @@
         </v-col>
       </v-row>
       <v-row>
-        <v-col
-          cols="12" md="4"
-        >
-          <v-btn
-            @click='addGenesis()'
-            class="mx-2"
-            dark
-            color="indigo"
-          >
-            <v-icon dark>
-              mdi-plus
-            </v-icon>
+        <v-col cols="12" md="4">
+          <v-btn @click="addGenesis()" class="mx-2" dark color="indigo">
+            <v-icon dark> mdi-plus </v-icon>
             add genesis account
           </v-btn>
         </v-col>
       </v-row>
     </v-container>
 
-    <v-btn
-      elevation="2"
-      type="submit"
-      class="my-6"
-    >Save Event</v-btn>
+    <v-btn elevation="2" type="submit" class="my-6">Save Event</v-btn>
   </v-form>
 </template>
 <script>
-import {mapActions} from 'vuex'
+import { mapActions } from "vuex";
 export default {
-  name: 'WorkspacesCreate',
-  data () {
+  name: "WorkspacesCreate",
+  data() {
     return {
       valid: false,
       model: {
@@ -147,37 +121,35 @@ export default {
           binary_url: "string",
           cli_path: "string",
           daemon_path: "string",
-          docker_image: "string"
+          docker_image: "string",
         },
         genesis_accounts: [
           {
             name: "Genesis Account #1",
             genesis_balance: "10000",
             faucet: true,
-            validator: true
-          }
+            validator: true,
+          },
         ],
-      }
-    }
+      },
+    };
   },
   methods: {
     submit() {
-      this.createEvent(this.model)
+      this.createEvent(this.model);
     },
     addGenesis() {
       this.model.genesis_accounts.push({
-        name: `Genesis Account #${this.model.genesis_accounts.length+1}`,
+        name: `Genesis Account #${this.model.genesis_accounts.length + 1}`,
         genesis_balance: "1000",
         validator: true,
-        faucet: true
-      })
+        faucet: true,
+      });
     },
     removeGenesis(n) {
-      this.$delete(this.model.genesis_accounts, n)
+      this.$delete(this.model.genesis_accounts, n);
     },
-    ...mapActions([
-      'createEvent'
-    ])
-  }
-}
+    ...mapActions(["createEvent"]),
+  },
+};
 </script>
