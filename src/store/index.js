d7f9caa8aa4dc27f97de5819f1f3c6dad17ebc24
Andrea Giacobino <no.andrea@gmail.com>
2020-12-23 10:09:40 +0100

    feat: redirect to events after delete

--- src/store/index.js
+++ src/store/index.js
@@ -3,43 +3,45 @@ import Vuex from 'vuex'
 import Cookies from 'js-cookie'
 
 const AUTH_TOKEN_COOKIE_NAME = process.env.VUE_APP_AUTH_TOKEN_COOKIE_NAME
-const AUTH_NAME_COOKIE_NAME  = process.env.VUE_APP_AUTH_NAME_COOKIE_NAME
+const AUTH_NAME_COOKIE_NAME = process.env.VUE_APP_AUTH_NAME_COOKIE_NAME
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    loading                : false,
-    status                 : {},
-    authToken              : Cookies.get(AUTH_TOKEN_COOKIE_NAME),
-    authName               : Cookies.get(AUTH_NAME_COOKIE_NAME),
-    events                 : []
+    loading: false,
+    status: {},
+    authToken: Cookies.get(AUTH_TOKEN_COOKIE_NAME),
+    authName: Cookies.get(AUTH_NAME_COOKIE_NAME),
+    events: []
   },
   mutations: {
-    SET_LOADING (state, loading) {
+    SET_LOADING(state, loading) {
       state.loading = loading
     },
-    SET_AUTH_TOKEN (state, token) {
+    SET_AUTH_TOKEN(state, token) {
       state.authToken = token
       Cookies.set(AUTH_TOKEN_COOKIE_NAME, token, {
         sameSite: 'strict',
-        expires: 7 })
+        expires: 7
+      })
     },
-    SET_AUTH_NAME (state, name) {
+    SET_AUTH_NAME(state, name) {
       state.authName = name
       Cookies.set(AUTH_NAME_COOKIE_NAME, name, {
         sameSite: 'strict',
-        expires: 7 })
+        expires: 7
+      })
     },
-    SET_STATUS (state, status) {
+    SET_STATUS(state, status) {
       state.status = status
     },
-    SET_EVENTS (state, events) {
+    SET_EVENTS(state, events) {
       state.events = events
     },
   },
   actions: {
-    loadHealth({commit},) {
+    loadHealth({ commit },) {
       const url = `status`
       commit('SET_LOADING', true)
       Vue.axios
@@ -56,7 +58,7 @@ export default new Vuex.Store({
           commit('SET_LOADING', false)
         })
     },
-    authLogin({commit}, payload) {
+    authLogin({ commit }, payload) {
       const url = `/v1/auth/login`
       Vue.axios
         .post(url, payload)
@@ -73,7 +75,7 @@ export default new Vuex.Store({
           commit('SET_LOADING', false)
         })
     },
-    authLogout({commit}) {
+    authLogout({ commit }) {
       const url = `/v1/auth/logout`
       Vue.axios
         .post(url)
@@ -92,7 +94,7 @@ export default new Vuex.Store({
           commit('SET_LOADING', false)
         })
     },
-    authRegister({commit}, payload) {
+    authRegister({ commit }, payload) {
       const url = `/v1/auth/register`
       Vue.axios
         .post(url, payload)
@@ -108,7 +110,7 @@ export default new Vuex.Store({
           commit('SET_LOADING', false)
         })
     },
-    loadEvents({commit}) {
+    loadEvents({ commit }) {
       const url = `/v1/events`
       Vue.axios
         .get(url)
@@ -124,9 +126,8 @@ export default new Vuex.Store({
           commit('SET_LOADING', false)
         })
     },
-    createEvent({commit}, payload) {
+    createEvent({ commit }, payload) {
       const url = `/v1/events`
-      Vue.axios
       return Vue.axios
         .post(url, payload)
         .then(r => r.data)
@@ -141,7 +142,7 @@ export default new Vuex.Store({
           commit('SET_LOADING', false)
         })
     },
-    loadEvent({commit}, id) {
+    loadEvent({ commit }, id) {
       const url = `/v1/events/${id}`
       Vue.axios
         .get(url)
@@ -157,9 +158,9 @@ export default new Vuex.Store({
           commit('SET_LOADING', false)
         })
     },
-    deleteEvent({commit}, id) {
+    deleteEvent({ commit }, id) {
       const url = `/v1/events/${id}`
-      Vue.axios
+      return Vue.axios
         .delete(url)
         .then(r => r.data)
         .then(data => {
@@ -173,7 +174,7 @@ export default new Vuex.Store({
           commit('SET_LOADING', false)
         })
     },
-    deployEvent({commit}, id) {
+    deployEvent({ commit }, id) {
       const url = `/v1/events/${id}/deploy`
       Vue.axios
         .put(url)
--- src/views/Event.vue
+++ src/views/Event.vue
@@ -1,40 +1,28 @@
 <template>
-  <v-card >
-    <v-toolbar
-      color="indigo"
-      dark
-      flat
-    >
+  <v-card>
+    <v-toolbar color="indigo" dark flat>
       <v-toolbar-title>
-        <h3>Event: {{id}}</h3>
+        <h3>Event: {{ id }}</h3>
       </v-toolbar-title>
       <template v-slot:extension>
-        <v-tabs
-          v-model="tab"
-          align-with-title
-        >
-          <v-tabs-slider ></v-tabs-slider>
-          <v-tab
-            v-for="item in items"
-            :key="item"
-          >
+        <v-tabs v-model="tab" align-with-title>
+          <v-tabs-slider></v-tabs-slider>
+          <v-tab v-for="item in items" :key="item">
             {{ item }}
           </v-tab>
         </v-tabs>
       </template>
     </v-toolbar>
     <v-tabs-items v-model="tab">
-      <v-tab-item
-        class="pa-4"
-      >
+      <v-tab-item class="pa-4">
         <h2>Settings</h2>
-        <p>id: {{event.id}}</p>
-        <p>token_symbol: {{event.token_symbol}}</p>
-        <p>owner: {{event.owner}}</p>
-        <p>provider: {{event.provider}}</p>
-        <p>created_on: {{event.created_on}}</p>
-        <p>starts_on: {{event.starts_on}}</p>
-        <p>ends_on: {{event.ends_on}}</p>
+        <p>id: {{ event.id }}</p>
+        <p>token_symbol: {{ event.token_symbol }}</p>
+        <p>owner: {{ event.owner }}</p>
+        <p>provider: {{ event.provider }}</p>
+        <p>created_on: {{ event.created_on }}</p>
+        <p>starts_on: {{ event.starts_on }}</p>
+        <p>ends_on: {{ event.ends_on }}</p>
 
         <h2>Accounts:</h2>
 
@@ -42,93 +30,63 @@
           class="my-4"
           elevation="6"
           v-for="a in event.accounts"
-          :key='a.name'
+          :key="a.name"
         >
-          <v-card-title>
-            address: {{a.address}}
-          </v-card-title>
+          <v-card-title> address: {{ a.address }} </v-card-title>
           <v-card-text>
-            <p>name: {{a.name}}</p>
-            <p>genesis_balance: {{a.genesis_balance}}</p>
-            <p>validator: {{a.validator}}</p>
-            <p>faucet: {{a.faucet}}</p>
+            <p>name: {{ a.name }}</p>
+            <p>genesis_balance: {{ a.genesis_balance }}</p>
+            <p>validator: {{ a.validator }}</p>
+            <p>faucet: {{ a.faucet }}</p>
           </v-card-text>
         </v-card>
       </v-tab-item>
-      <v-tab-item
-        class="pa-4"
-      >
+      <v-tab-item class="pa-4">
         <h2>Deploy</h2>
-        <p>
-          Deploy can not be undone.
-        </p>
-        <v-btn
-          @click="deploy"
-          dark
-          color="green"
-        >
-          Deploy
-        </v-btn>
+        <p>Deploy can not be undone.</p>
+        <v-btn @click="deploy" dark color="green"> Deploy </v-btn>
       </v-tab-item>
 
-      <v-tab-item
-        class="pa-4"
-      >
+      <v-tab-item class="pa-4">
         <h2>Delete</h2>
         <p>Deleting can not be undone.</p>
-        <v-btn
-          @click="destroy"
-          dark
-          color="red"
-        >
-          Delete
-        </v-btn>
+        <v-btn @click="destroy" dark color="red"> Delete </v-btn>
       </v-tab-item>
     </v-tabs-items>
   </v-card>
 </template>
 <script>
-import { mapActions, mapState} from 'vuex'
+import { mapActions, mapState } from "vuex";
 export default {
-  name: 'Event',
-  data () {
+  name: "Event",
+  data() {
     return {
       tab: null,
-      items: [
-        'Settings',
-        'Deploy',
-        'Delete'
-      ],
-    }
+      items: ["Settings", "Deploy", "Delete"],
+    };
   },
   computed: {
     id() {
-      return this.$route.params.id
+      return this.$route.params.id;
     },
     event() {
-      return this.events.filter(e=>e.id===this.id)[0]
+      return this.events.filter((e) => e.id === this.id)[0];
     },
-    ...mapState([
-      'events'
-    ])
+    ...mapState(["events"]),
   },
   methods: {
     destroy() {
-      if(confirm('Are you sure you want to delete this event?'))
-        this.deleteEvent(this.id)
+      if (confirm("Are you sure you want to delete this event?"))
+        this.deleteEvent(this.id).then(this.$router.push({ path: "/events" }));
     },
     deploy() {
-      if(confirm('Are you sure you want to deploy this event?'))
-        this.deployEvent(this.id)
+      if (confirm("Are you sure you want to deploy this event?"))
+        this.deployEvent(this.id);
     },
-    ...mapActions([
-      'loadEvent',
-      'deleteEvent',
-      'deployEvent'
-    ])
+    ...mapActions(["loadEvent", "deleteEvent", "deployEvent"]),
   },
   mounted() {
-    this.loadEvent(this.id)
-  }
-}
+    this.loadEvent(this.id);
+  },
+};
 </script>
