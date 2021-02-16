e918656e82843a133db09c99bd339387a21bef83
gabrielaacha <57134592+gabrielaacha@users.noreply.github.com>
2021-02-16 17:50:23 +0100

    added texts explaining whats going in create event

--- src/views/EventsCreate-2.vue
+++ src/views/EventsCreate-2.vue
@@ -1,87 +1,51 @@
 <template>
   <v-form @submit.prevent="submit" v-model="valid" ref="form">
-    <v-container class="d-flex align-center">
-     <v-container class="d-flex justify-start">    
-     <h1>New Infrastructure</h1> 
-     </v-container>
+      <v-container class="d-flex align-center">
+     <v-container class="d-flex justify-start">
+    <h1>Create new Event Infrastructure</h1>
+      </v-container>
        <v-container class="d-flex justify-end">
-    <v-dialog
-      v-model="dialog"
-      persistent
-      max-width="390"
-    >
-      <template v-slot:activator="{ on, attrs }">
-         <v-btn
-         elevation="2"
-         type="submit"
-         class="my-6 d-flex justify-end mr-2"
-         outlined
-         :loading="loading"
-         >Save Event</v-btn>
-        
-        <v-btn
-          v-bind="attrs"
-          v-on="on"
-          elevation="2"
-          class="my-6 d-flex justify-end"
-          outlined
-        >
-          Cancel
-        </v-btn>
-      </template>
-
-      <v-card class="px-6 py-4">
-        <v-card-title class="headline">
-       Are you sure that you want to cancel the new event?        
-      </v-card-title>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="black"
-            outlined
-            text
-            type="submit"
-          @click.prevent="cancel"  
-           :loading="loading">     
-            yes
-          </v-btn>
-         
-         <v-btn
-            color="black"
-            outlined
-            text
-            @click="dialog = false"
-          >
-          No          
-          </v-btn>
-        </v-card-actions>
+       <v-btn
+      elevation="2"
+      type="submit"
+      class="my-6 d-flex justify-end mr-2"
+       outlined
+      :loading="loading"
+    >Save Event</v-btn>
+    <v-btn
+    @click.prevent="cancel" 
+      elevation="2"
+      type="submit"
+      class="my-6 d-flex justify-end"
+       outlined
+      :loading="loading"
+    >Cancel</v-btn>
+     </v-container>
+     </v-container>
+      <v-card 
+      class="pa-6 mb-6">    
+      <p>Here you can set the presets of your event infrastructure. This doesn’t mean, that your infrastructure is fully deployed yet. After Saving you have to go to the settings of the created infrastructure and fully deploy it there!</p> 
       </v-card>
-    </v-dialog>
-     </v-container>
-     </v-container>
 
-    <v-card class="pa-6 mb-6">
-     <v-card-title>Create your event Infrastructure</v-card-title>
-     <p class="px-4 pb-4">Here you can set the presets of your event infrastructure. 
-       This doesn’t mean, that your infrastructure is fully deployed yet. 
-       After Saving you have to go to the settings of the created infrastructure and fully deploy it there!</p> 
-     </v-card>
-
-    <v-sheet>
-    <v-card class="my-4 pa-6">
-      <v-card-title>Give a name to your token symbol</v-card-title>
-      <p class="px-4">As part of your event infrastructure tokens will be created. Please choose a name for your tokens.</p>
+      <v-sheet>
+    <v-card class="mb-4 pa-6">
+      <v-card-title>Event</v-card-title>
       <v-card-text>
-        <v-container class="border">
+        <v-container>
+          <!-- <v-row justify="space-between">
+            <v-col cols="12" md="4"> -->
               <v-text-field
                 v-model="model.token_symbol"
                 label="Token Symbol"
-                value="DROPS"
+                value="drops"
               ></v-text-field>
+            <!-- </v-col>
+          </v-row> -->
         </v-container>
       </v-card-text>
     </v-card>
-    <v-card
+    <!-- </v-sheet> -->
+    <!-- <v-card
       class="my-4"
       v-if='false'
     >
@@ -114,44 +78,40 @@
           </v-row>
         </v-container>
       </v-card-text>
-    </v-card>
-    </v-sheet>
-    <v-container class="border">
-      <v-row class="border">
+    </v-card> -->
+     </v-sheet>
+
+    <v-container>
+          <v-row>
         <v-col cols="12" md="12">
+           
           <v-card 
-          class="pa-6 border"
+          class="pa-6"
           elevation="2">
-          <v-card-title>Genesis Accounts</v-card-title>
-         <p class="px-5 pb-2 border">
-          Genesis accounts can control the event infrastructure. 
-          Please provide an email address of all users who should be able to control the 
-          event infrastructure. Genesis accounts also receive the amount of tokens specified 
-          in these settings. From the Genesis accounts, the tokens can be distributed 
-          to your event participants.</p> 
+         <p>
+          Here you can set the presets of your event infrastructure. This doesn’t mean, that your infrastructure is fully deployed yet. After Saving you have to go to the settings of the created infrastructure and fully deploy it there!</p> 
           <v-btn @click="addGenesis()" 
-          class="mx-5 mb-6" 
+          class="mx-2" 
           outlined 
-          elevation="2"
-          color="blue darken-4">
+          elevation="2">
             <v-icon dark> mdi-plus </v-icon>
-            additional genesis account
+            add genesis account
           </v-btn>
           </v-card>
+      
         </v-col>
       </v-row>
       <v-row>
         <v-col
           cols="12"
-          md="6"
+          md="4"
           v-for="(g, n) in model.genesis_accounts"
           :key="n"
-        >
-        <v-sheet rounded elevation="2">
-          <v-expansion-panels class="mb-4">
-           <v-card class="pa-6 border">
-           <v-card-text class="black--text">
-              <h3 class="pt-2">New Genesis Account</h3>
+        > 
+         <v-sheet rounded elevation="2">
+          <v-card class="pa-6">
+            <v-card-text color="black">
+              <h3 class="pt-2" color="black">Genesis Account</h3>
               <p class="py-4">
               As part of your event infrastructure tokens will be created. Please choose a name for your tokens.</p>
               <v-text-field v-model="g.name" label="E-Mail"                 
@@ -163,17 +123,14 @@
                 :suffix="`${model.token_symbol}`"
                 label="Genesis Token Balance"
               ></v-text-field>
-            <v-expansion-panel class="mb-6 border-none rounded-lg">
-          <v-expansion-panel-header>Further options</v-expansion-panel-header>
-        <v-expansion-panel-content>
-           <v-text-field
-               class="py-4"
-               v-model="g._amount_gas"
-               label="Gas Token Balance"
-               :suffix="`${token_gas_symbol}`"
-               hint="balance for tokens used to pay for gas"
-          > </v-text-field>
-            <v-text-field
+              <v-text-field
+                class="py-4"
+                v-model="g._amount_gas"
+                label="Gas Token Balance"
+                :suffix="`${token_gas_symbol}`"
+                hint="balance for tokens used to pay for gas"
+              ></v-text-field>
+              <v-text-field
                 class="py-4"
                 v-model="g._amount_stake"
                 label="Stake"
@@ -181,7 +138,8 @@
                 hint="stake assigned to the account"
               ></v-text-field>
                 <v-sheet class="pb-8 pl-2">
-              <v-switch
+              <v-switch 
+                border="black"
                 v-model="g.faucet"
                 inset
                 :label="`Is Faucet`"
@@ -193,25 +151,20 @@
                 :label="`Is validator`"
                 hint="the account will be a validator"
               ></v-switch>
-                </v-sheet>        
-        </v-expansion-panel-content>
-        </v-expansion-panel>
-          <v-btn @click="removeGenesis(n)" 
+                </v-sheet>
+              <v-btn @click="removeGenesis(n)" 
               outlined
-              class="mx-2 my-4" 
-              dark color="red" 
-              elevation="2">
+               elevation="2"
+              class="mx-2" justify="center" dark color="red" >
                 <v-icon dark> mdi-minus </v-icon>
                 remove
-           </v-btn>
-         </v-card-text>
-         </v-card>
-        </v-expansion-panels>
-        </v-sheet>
+              </v-btn>
+            </v-card-text>
+          </v-card>
+          </v-sheet>
         </v-col>
       </v-row>
-      <!-- </v-card> -->
-    </v-container>  
+    </v-container>
   </v-form>
 </template>
 <script>
@@ -220,12 +173,12 @@ export default {
   name: "WorkspacesCreate",
   data() {
     return {
-      dialog: false,
       valid: false,
       token_gas_symbol: "evtx",
       token_stake_symbol: "stake",
       model: {
-        token_symbol: "DROPS",
+        owner: "",
+          token_symbol: "drops",
         genesis_accounts: [
         ],
       },
@@ -294,13 +247,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.border {
-  border: solid black 1px,
-}
-
-.border-none {
-  border: none,
-}
-</style>
